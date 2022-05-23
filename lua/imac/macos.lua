074d83517aa45fbe8f1a099dd5a1126aa4ccdd42
ds1sqe <ds1sqe@mensakorea.org>
2022-05-23 12:03:01 +0900

    macos support with im-select

--- lua/imac/macos.lua
+++ lua/imac/macos.lua
@@ -23,7 +23,10 @@ end
 
 function M.set()
   if vim.g.IME_ENG=="" then
-    vim.g.IME_ENG="com.apple.keylayout.US"
+    vim.g.IME_ENG="com.apple.keylayout.ABC"
+  end if vim.g.IME_Target=="" then
+    vim.g.IME_Target = "com.apple.inputmethod.Korean.2SetKorean"
+
   end
   if os.execute("which im-select")==1 then
     os.execute("brew tap daipeihust/tap && brew install im-select")
